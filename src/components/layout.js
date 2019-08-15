b09700db1fc558ffc123ae4acbece404e46d5d3e
David Gaskin <davidburkegaskin@gmail.com>
2019-08-15 00:29:37 -0700

    implement basic layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,37 +12,39 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
+const Layout = (props) => {
+   const data = useStaticQuery(graphql`
+      query SiteMetaQuery {
+         wordpressSiteMetadata {
+           home
+           name
+           description
+         }
       }
-    }
-  `)
-
+   `)
+  const { home, name, description } = data.wordpressSiteMetadata
+  console.dir(data.wordpressSiteMetadata);
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+     <>
+        <div id="bodyTag" className={props.className}>
+           <Header name={name}/>
+           <main>
+              <p>{description}</p>
+              {props.children}
+           </main>
+
+           {/*<Footer slug={props.slug} />*/}
+           <footer>
+             © {new Date().getFullYear()}, Built with
+             {` `}
+             <a href="https://www.gatsbyjs.org">Gatsby</a>
+           </footer>
+
+           {/*<Modal path={path}/>*/}
+        </div>
+     </>
   )
+
 }
 
 Layout.propTypes = {
